0c7d8e25e98a5e36eab88efa28efca1a5630170e
Rogerio da Silva <rogerleonino@gmail.com>
2024-07-30 09:12:41 -0300

    Criar modelos iniciais com refatoração e melhorias de performance
    - Adicionada a criação inicial dos seguintes modelos:
      - MessageUser: Model para mensagens entre usuários com campos renomeados e otimização de dados.
      - PaletaCores: Model para definir paletas de cores com campos ajustados para melhor consistência.
      - NomeclaturaJanelas: Model para nomenclaturas de janelas com ajustes no nome do campo.
    
    - Implementado sinal post_migrate para:
      - Criar mensagens de boas-vindas para todos os usuários, se ainda não existirem.
      - Configurar registros padrão no modelo NomeclaturaJanelas, se não estiverem presentes.
      - Garantir que grupos necessários sejam criados ou obtidos no sinal post_migrate.
    
    - Adicionado erbose_name e erbose_name_plural aos modelos para melhorar a legibilidade no admin.
    
    Essas alterações estabelecem a base para a gestão de mensagens, paletas de cores e nomenclaturas de janelas, melhorando a organização e a eficiência do sistema.

---
+++ admin_acessos/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_migrate
+from ckeditor.fields import RichTextField
+
+
+class MessageUser(models.Model):
+    remetente = models.ForeignKey(User, null=True, on_delete=models.CASCADE, editable=False, verbose_name="Remetente da mensagem", related_name="sent_messages")
+    destinatario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Destinatário da mensagem", related_name="received_messages")
+    assunto = models.CharField(max_length=100, verbose_name='Assunto da mensagem')
+    mensagem = RichTextField(null=True, blank=True)
+    aberta = models.BooleanField(default=False)
+    foi_consultado = models.BooleanField(default=False)
+    data_envio = models.DateTimeField(auto_now_add=True)
+    exclude_msg = models.CharField(null=True, max_length=5)
+
+    class Meta:
+        ordering = ["-data_envio"]
+        verbose_name = "Mensagem"
+        verbose_name_plural = "Mensagens"
+
+    def __str__(self) -> str:
+        return self.assunto
+
+
+class PaletaCores(models.Model):
+    nome_paleta = models.CharField(max_length=20, default='Paleta Branca')
+    cor_primaria = models.CharField(max_length=7, default='#fff')
+    cor_secundaria = models.CharField(max_length=7, default='#fff')
+    cor_sucesso = models.CharField(max_length=7, default='#fff')
+    cor_info = models.CharField(max_length=7, default='#fff')
+    cor_aviso = models.CharField(max_length=7, default='#fff')
+    cor_perigo = models.CharField(max_length=7, default='#ffffff')
+    cor_texto = models.CharField(max_length=7, default='#000')
+
+    def __str__(self):
+        return self.nome_paleta
+
+
+class NomeclaturaJanelas(models.Model):
+    nome_disciplina = models.CharField(max_length=50, default='')
+    notas = models.CharField(max_length=20, default='')
+
+    def __str__(self):
+        return self.nome_disciplina
+
+
+@receiver(post_migrate)
+def setup_post_migrate(sender, **kwargs):
+    if sender.name == 'admin_acessos':
+        if not MessageUser.objects.exists():
+            for user in User.objects.all():
+                MessageUser.objects.create(
+                    destinatario=user,
+                    assunto="Olá!",
+                    mensagem="Bem-vindo ao nosso sistema!",
+                )
+
+    if not NomeclaturaJanelas.objects.exists():
+        NomeclaturaJanelas.objects.create(
+            nome_disciplina='Objetos da Aprendizagem/Disciplinas',
+            notas='Notas do Aluno'
+        )
+    
+    # Adicionar ou modificar grupos se necessário
+    group_names = ['Nutricionista', 'Professor', 'Diretor', 'Aluno']
+    for group_name in group_names:
+        Group.objects.get_or_create(name=group_name)
