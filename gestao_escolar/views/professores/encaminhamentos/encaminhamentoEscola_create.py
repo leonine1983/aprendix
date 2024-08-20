7ccb5fadb8914323a6e79edceff33551e86475a4
Rogerio da Silva <rogerleonino@gmail.com>
2024-08-20 14:41:01 -0300

    novo

--- gestao_escolar/views/professores/encaminhamentos/encaminhamentoEscola_create.py
+++ gestao_escolar/views/professores/encaminhamentos/encaminhamentoEscola_create.py
@@ -5,11 +5,12 @@ from django.views.generic import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from rh.models import Encaminhamentos, Escola, Contrato
-from django.urls import reverse_lazy
+from rh.models import Encaminhamentos, Escola, Contrato,Ano
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
 
 
-class EncaminhaEscola (LoginRequiredMixin, CreateView, SuccessMessageMixin):
+class EncaminhaEscola(LoginRequiredMixin, CreateView, SuccessMessageMixin):
     model = Encaminhamentos
     fields = ['profissao']
     success_message = "Profissional Encaminhado para a sua escola com sucesso, para o ano letivo atual!!"
@@ -18,13 +19,44 @@ class EncaminhaEscola (LoginRequiredMixin, CreateView, SuccessMessageMixin):
     def form_valid(self, form: BaseModelForm):
         escola_id = self.request.session['escola_id']
         ano_letivo = self.request.session['anoLetivo_id']
-        print(f'anoletivo {ano_letivo}')
         pk = self.kwargs['pk']
-        escola =  Escola.objects.get(id = escola_id)
-        contrato = Contrato.objects.get(id = pk, ano_contrato = ano_letivo)
-        form.instance.encaminhamento = contrato
-        form.instance.destino = escola
-        form.save()               
+
+        contrato_get = Contrato.objects.get(id=pk)
+
+        print(f'o que tem no form {form}')
+        
+        if contrato_get.ano_contrato.id != ano_letivo:
+            if not Contrato.objects.filter(ano_contrato=ano_letivo).exists():
+                novo_contrato = Contrato.objects.create(
+                    ano_contrato=Ano.objects.get(id=ano_letivo),
+                    contratado=contrato_get.contratado,
+                    text_contrato=contrato_get.text_contrato,
+                    nome_profissao=contrato_get.nome_profissao,
+                    nome_escola=Escola.objects.get(id=escola_id),
+                    salario=contrato_get.salario,
+                    tempo_meses=contrato_get.tempo_meses
+                )
+                novo_contrato.save()
+                messages.success(self.request, f"Novo contrato para o {novo_contrato.contratado} para o ano de {novo_contrato.ano_contrato} criado com sucesso!!")
+
+                escola = Escola.objects.get(id=escola_id)
+                form.instance.encaminhamento = novo_contrato
+                form.instance.destino = escola
+                form.instance.profissao = contrato_get.nome_profissao  # Ajuste conforme necessário
+                form.save()
+                
+                return redirect(reverse('Gestao_Escolar:Professores_Pessoa_create'))
+
+        else:
+            escola = Escola.objects.get(id=escola_id)
+            contrato = Contrato.objects.get(id=pk, ano_contrato=ano_letivo)
+            form.instance.encaminhamento = contrato
+            form.instance.destino = escola
+            form.instance.profissao = contrato_get.nome_profissao  # Ajuste conforme necessário
+            form.save()
+            
+            return redirect(reverse('Gestao_Escolar:Professores_Pessoa_create'))
+
         return super().form_valid(form)
     
     def get_success_url(self):
