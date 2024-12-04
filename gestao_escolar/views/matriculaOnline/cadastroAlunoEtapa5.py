3604da1648c6177b2ee2beeb828b6b0119ecc3e9
Rogerio da Silva <rogerleonino@gmail.com>
2024-12-04 11:32:22 -0300

    novo

--- gestao_escolar/views/matriculaOnline/cadastroAlunoEtapa5.py
+++ gestao_escolar/views/matriculaOnline/cadastroAlunoEtapa5.py
@@ -10,16 +10,17 @@ def cadastro_aluno_etapa5(request, aluno_id):
     aluno = Alunos.objects.get(id=aluno_id)
     form = MatriculaOnline_etapa5(request.POST or None, instance=aluno)
     if request.method == 'POST':
-        # Aqui você pode adicionar mais dados, como dados de saúde, escolaridade, etc.
-        aluno.espectro_autista = request.POST.get('espectro_autista')
-        aluno.deficiencia_aluno = request.POST.get('deficiencia_aluno')
-        aluno.tipo_sanguineo = request.POST.get('tipo_sanguineo')
-        aluno.deficiencia_aluno = request.POST.get('necessita_edu_especial')
-        aluno.vacina_covid_19 = request.POST.get('vacina_covid_19')
-        aluno.dose_vacina_covid_19 = request.POST.get('dose_vacina_covid_19')
-        aluno.sindrome_de_Down = request.POST.get('sindrome_de_Down')
-        aluno.save()
-        messages.success(request, f"As informações foram atualizadas com sucesso: RG e CPF. A partir de agora, você atualizarar os dados da Certidão de Nascimento do aluno.")
-        return redirect('matricula_online', aluno_id=aluno.id)      
+        if form.is_valid():
+            # Aqui você pode adicionar mais dados, como dados de saúde, escolaridade, etc.
+            aluno.espectro_autista = form.cleaned_data['espectro_autista']
+            aluno.deficiencia_aluno = form.cleaned_data['deficiencia_aluno']
+            aluno.tipo_sanguineo = form.cleaned_data['tipo_sanguineo']
+            aluno.deficiencia_aluno = form.cleaned_data['necessita_edu_especial']
+            aluno.vacina_covid_19 = form.cleaned_data['vacina_covid_19']
+            aluno.dose_vacina_covid_19 = form.cleaned_data['dose_vacina_covid_19']
+            aluno.sindrome_de_Down = form.cleaned_data['sindrome_de_Down']
+            aluno.save()
+            messages.success(request, f"As informações foram atualizadas com sucesso: Fisicos e Biológicos. A partir de agora, você já pode realizar a matrícula do aluno em uma das turmas disponíveis")
+            return redirect('matricula_online', aluno_id=aluno.id)      
     
     return render(request, 'Escola/matriculaOnline/etapa5.html', {'aluno': aluno, 'form':form})
--- gestao_escolar/views/matriculaOnline/matriculaOnline.py
+++ gestao_escolar/views/matriculaOnline/matriculaOnline.py
@@ -17,3 +17,14 @@ def matricular_aluno(request, aluno_id):
         return render(request, 'matricula_online/matricula_confirmada.html', {'aluno': aluno})
 
     return render(request, 'Escola/matriculaOnline/matricular_aluno.html', {'aluno': aluno})
+"""
+
+class MatriculasOnline(models.Model):    
+    cod_matriculaOline = models.TextField(max_length=200, null=True, default='2025-001')
+    aluno = models.ForeignKey(Alunos, related_name='related_matriculaOnline_alunos', on_delete=models.CASCADE)
+    serie =  models.ForeignKey(Serie_Escolar, related_name="related_serie_matricula", on_delete=models.CASCADE)
+    ano_letivo = models.ForeignKey(AnoLetivo, related_name="related_anoLetivoOnline", on_delete=models.CASCADE)
+    matriculaConfirmada = models.BooleanField(default=False)
+    escola = models.ForeignKey('rh.Escola', related_name='OnlineMatriculaEscola', on_delete=models.CASCADE)
+
+"""
