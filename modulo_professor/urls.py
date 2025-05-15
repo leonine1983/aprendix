3714e32c358bade95655e382bcd2c8905483218a
Rogério da Silva <rogerleonino@gmail.com>
2025-05-15 09:09:53 -0300

    todos

--- modulo_professor/urls.py
+++ modulo_professor/urls.py
@@ -10,12 +10,12 @@ app_name = 'modulo_professor'
 urlpatterns = [
     path('', home_professor, name='homeProfessor'),
     path('/sessao/', home_sessaoIniciada, name='sessaoEscola'),
-    path('notas/criar/<int:aluno>/<int:grade>/<int:trimestre>', criaNotasComposicao, name='criaNotasComposicao'),
-    path('recuperacao-final/<int:pk>/<int:grade>/', atualizaRecuperaFinal, name='atualiza_recuperacao_final'),
+    path('/notas/criar/<int:aluno>/<int:grade>/<int:trimestre>', criaNotasComposicao, name='criaNotasComposicao'),
+    path('/recuperacao-final/<int:pk>/<int:grade>/', atualizaRecuperaFinal, name='atualiza_recuperacao_final'),
 
-    path('presenca/diaria/seleciona/', selecionaTurma, name='selecionaTurmaFalta'),
-    path('presenca/diaria/<int:turma_id>/', registrar_presenca_diaria_view, name='presenca_diaria'),
-    path('turma/<int:turma_id>/presencas/<str:data_str>/', lista_presenca_diaria_view, name='lista_presenca_diaria'),
+    path('/presenca/diaria/seleciona/', selecionaTurma, name='selecionaTurmaFalta'),
+    path('/presenca/diaria/<int:turma_id>/', registrar_presenca_diaria_view, name='presenca_diaria'),
+    path('/turma/<int:turma_id>/presencas/<str:data_str>/', lista_presenca_diaria_view, name='lista_presenca_diaria'),
 
     path('faltas/<int:matricula_id>/', historico_faltas_view, name='historico_faltas'),
 
