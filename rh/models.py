0a80c3e0f3ecf679b68a6041f385743d50e85781
Rogério da Silva <rogerleonino@gmail.com>
2025-07-17 14:10:16 -0300

    totwu

--- rh/models.py
+++ rh/models.py
@@ -585,11 +585,11 @@ class Frequencia_mes(models.Model):
 
     def __str__(self):
         return self.mes   
-
+"""
 # REGISTROS INICIAS ------------------------------------------
 @receiver(post_migrate)
 def post_migrate_setup(sender, **kwargs):
-    if sender.name != 'rh':  # Substitua 'rh' pelo nome do seu app
+    if sender.name != 'rh':  
         return
     
     
@@ -761,3 +761,4 @@ def post_migrate_setup(sender, **kwargs):
                     print(f"Escola_admin criado para: {escola.nome_escola}")
                 else:
                     print(f"Já existe um Escola_admin para: {escola.nome_escola}")
+"""
