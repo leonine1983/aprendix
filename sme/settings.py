46cdbc8d23bbf351d417b908ea1848a8f66588e4
Rogério da Silva <rogerleonino@gmail.com>
2025-07-23 11:50:16 -0300

    whitenoi

--- sme/settings.py
+++ sme/settings.py
@@ -49,8 +49,9 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [    
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,6 +155,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Mudanças do CKEDITOR
 # Media_path
