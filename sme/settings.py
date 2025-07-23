fa443f8fffca4b56052870952dd549bd490f5195
Rogério da Silva <rogerleonino@gmail.com>
2025-07-23 10:20:49 -0300

    tes tede debu

--- sme/settings.py
+++ sme/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-
+print(f"debug e: {DEBUG}")
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='*')
 
 #ALLOWED_HOSTS = ['*']
