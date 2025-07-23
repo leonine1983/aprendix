c630bea9508c6d23b86b5f31f93dd8fb25386b74
Rogério da Silva <rogerleonino@gmail.com>
2025-07-23 13:46:41 -0300

    debug

--- sme/settings.py
+++ sme/settings.py
@@ -6,8 +6,6 @@ import os
 from decouple import config, Csv
 
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,9 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
-print(f"debug e: {DEBUG}")
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='*')
 
 #ALLOWED_HOSTS = ['*']
