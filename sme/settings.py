40c8dc361fd02c6c7ff5e3d76ef8f56ff5b8b802
Rogério da Silva <rogerleonino@gmail.com>
2025-07-23 10:22:20 -0300

    test

--- sme/settings.py
+++ sme/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+#DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 print(f"debug e: {DEBUG}")
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='*')
 
