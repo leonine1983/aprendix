c3da5f59dc3d725e6e599cc9ccd58c3365b21c4c
Rogério da Silva <rogerleonino@gmail.com>
2025-09-30 15:06:30 -0300

    feat(ui): criar layout responsivo em Bootstrap para área do aluno com turma destacada
    - Adicionado container para melhor alinhamento
    - Header com gradiente exibindo {{matricula_atual.turma}} em destaque
    - Cards responsivos para próximas aulas, avisos e atalhos
    - Inclusão de abas (materiais, tarefas e calendário) para navegação

--- modulo_aluno/views.py
+++ modulo_aluno/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from gestao_escolar.models import MatriculasOnline
+from gestao_escolar.models import MatriculasOnline, Matriculas
+from rh.models import Ano
 
 
 # Create your views here.
 @login_required
 def home_aluno(request):
     userAluno = request.user.userAluno_related
-    request.session['alunoUser'] = userAluno.id
-
+    request.session['alunoUser'] = userAluno.id  
     aluno = userAluno.aluno.id
+    ano = Ano.objects.all().first()
+    matricula = Matriculas.objects.get(aluno = aluno, turma__ano_letivo = ano)   
+    matriculas = Matriculas.objects.filter(aluno = aluno)   
 
     # Pequisa pra verifica se existe matricula feita do aluno
     alunoMatricula = MatriculasOnline.objects.filter(aluno=aluno)
 
-    return render(request, 'modulo_aluno/base.html', {'aluno':alunoMatricula})
+    return render(request, 'modulo_aluno/base.html', {
+        'aluno':alunoMatricula,
+        'matricula_atual':matricula,
+        'matriculas':matriculas})
+
