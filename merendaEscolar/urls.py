c626493c4170ee520f59b8d9735e3d4a72244fc5
Rogério da Silva <rogerleonino@gmail.com>
2026-03-02 15:23:20 -0300

    mm

--- merendaEscolar/urls.py
+++ merendaEscolar/urls.py
@@ -6,6 +6,7 @@ from . views import *
 app_name = 'merendaEscolar'
 urlpatterns = [    
     #path('', inicio_merenda, name='merenda_inicio'),  
+    path('', EstoqueCentralListView, name='merenda_inicio'),  
     path("unidades/", UnidadeMedidaListView.as_view(), name="unidade_medida_list"),
     path("unidades/nova/", UnidadeMedidaCreateView.as_view(), name="unidade_medida_create"),
     path("unidades/<int:pk>/editar/", UnidadeMedidaUpdateView.as_view(), name="unidade_medida_update"),
@@ -22,9 +23,11 @@ urlpatterns = [
 
     # Estoque ----------------
      # Dashboard / Listagem
-     
-    #path("central/",EstoqueCentralListView.as_view(), name="estoque-central" ),
-    path("central/",EstoqueCentralListView.as_view(), name="merenda_inicio" ),
+    path(
+        "central/",
+        EstoqueCentralListView.as_view(),
+        name="estoque-central"
+    ),
 
     # Entrada de produtos no estoque central
     path(
