270f76d548f5ac407d2401f7306c233bc1659c94
Rogério da Silva <rogerleonino@gmail.com>
2026-04-22 14:54:25 -0300

    ficha dia

---
+++ modulo_Merendeiras/views/executarCardapio/fichaDiariaCreateView.py
@@ -0,0 +1,101 @@
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.db import transaction
+from django.contrib import messages
+
+from ...models import FichaDiaria, ExecucaoCardapioDia
+from ..baseMerendeiraView import BaseMerendeiraView
+
+from django import forms
+
+
+class FichaDiariaForm(forms.ModelForm):
+
+    class Meta:
+        model = FichaDiaria
+        fields = [
+            "alunos_atendidos",
+            "houve_alteracao_cardapio",
+            "cardapio_executado",
+            "aceitabilidade",
+            "houve_sobras",
+            "motivo_sobras",
+            "falta_alimento",
+            "problema_equipamento",
+            "falta_gas",
+            "outra_ocorrencia",
+            "observacoes",
+        ]
+
+        widgets = {
+            "alunos_atendidos": forms.NumberInput(attrs={
+                "class": "form-control",
+                "placeholder": "Quantidade de alunos atendidos"
+            }),
+            "houve_alteracao_cardapio": forms.CheckboxInput(attrs={
+                "class": "form-check-input"
+            }),
+            "cardapio_executado": forms.Textarea(attrs={
+                "class": "form-control",
+                "rows": 3
+            }),
+            "aceitabilidade": forms.Select(attrs={
+                "class": "form-select"
+            }),
+            "houve_sobras": forms.Select(attrs={
+                "class": "form-select"
+            }),
+            "motivo_sobras": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Motivo das sobras (se houver)"
+            }),
+            "outra_ocorrencia": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Descreva outra ocorrência"
+            }),
+            "observacoes": forms.Textarea(attrs={
+                "class": "form-control",
+                "rows": 3
+            }),
+        }
+
+
+class FichaDiariaCreateView(BaseMerendeiraView, CreateView):
+    model = FichaDiaria
+    form_class = FichaDiariaForm
+    template_name = "modulo_merendeiras/cadapioHoje/ficha_diaria.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.execucao = ExecucaoCardapioDia.objects.get(pk=self.kwargs["execucao_id"])
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["execucao"] = self.execucao
+        return ctx
+
+    def form_valid(self, form):
+        try:
+            with transaction.atomic():
+
+                ficha = form.save(commit=False)
+                ficha.execucao = self.execucao
+                ficha.escola = self.execucao.escola
+                ficha.data = self.execucao.data
+                ficha.turno = self.execucao.turno
+                ficha.tecnico_responsavel = self.request.user
+                ficha.save()
+
+                messages.success(
+                    self.request,
+                    "Ficha diária registrada com sucesso."
+                )
+
+        except Exception as e:
+            messages.error(self.request, f"Erro ao salvar ficha: {str(e)}")
+            return self.form_invalid(form)
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("modulo_merendeiras:historico_execucoes")
