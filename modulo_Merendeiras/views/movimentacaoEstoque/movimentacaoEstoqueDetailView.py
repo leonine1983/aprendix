6fd86c07eb079e404963ba4a0e88b6e876bc8ef0
Rogério da Silva <rogerleonino@gmail.com>
2026-05-21 13:40:26 -0300

    todos

--- modulo_Merendeiras/views/movimentacaoEstoque/movimentacaoEstoqueDetailView.py
+++ modulo_Merendeiras/views/movimentacaoEstoque/movimentacaoEstoqueDetailView.py
@@ -3,23 +3,15 @@ from django.contrib import messages
 from django.db.models import F
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from core.permissions import GroupRequiredMixin
-from core.groups.merenda import MERENDEIRA_GROUPS
-from ..baseMerendeiraView import BaseMerendeiraView
-
+from core.views.baseMerendeira import BaseMerendeiraView
 from merendaEscolar.models import MovimentacaoEstoque
 
 
-class MovimentacaoEstoqueDetailView(
-    LoginRequiredMixin,
-    GroupRequiredMixin,
-    DetailView
-):
+class MovimentacaoEstoqueDetailView(BaseMerendeiraView, DetailView):
     model = MovimentacaoEstoque
     template_name = "modulo_merendeiras/movimentaEstoque/movimentacao_detail.html"
     context_object_name = "movimentacao"
 
-    group_required = MERENDEIRA_GROUPS  # ajustar depois
 
     def get(self, request, *args, **kwargs):
         messages.info(request, "Detalhes da movimentação carregados.")
